c4ae2dc2382538bb3a8d70fc823ee093e8f0be85
BeZen13 <55999473+BeZen13@users.noreply.github.com>
2019-12-12 21:52:03 -0700

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+function Header (){
+    return(
+    <header>
+        <img 
+        src="https://unsplash.com/photos/k8OCHhEymME"
+        alt="Oh SHiat"
+        />
+        <p>Epic Meme Generator</p>
+    </header>
+    
+    )
+}
+export default Header
